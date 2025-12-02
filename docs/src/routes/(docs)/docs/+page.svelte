ebdc5023a5924e7052e32039092b9ebdf94449f6
theetherGit <meenashivam9650@gmail.com>
2025-12-02 12:08:15 +0530

    chore(docs): update the uses in docs

--- docs/src/routes/(docs)/docs/+page.svelte
+++ docs/src/routes/(docs)/docs/+page.svelte
@@ -1,6 +1,24 @@
 <script lang="ts">
 	import { DocPage } from "@svecodocs/kit";
+    import {getNavigationNeighbors} from "$lib/navigation";
+    import {page} from "$app/state";
+    import type {ComponentProps} from "svelte";
 	let { data } = $props();
+
+    const contributors: ComponentProps<typeof DocPage>['contributors'] = [
+        {
+            login: 'theetherGit',
+            avatar_url: 'https://avatars.githubusercontent.com/u/71087863',
+            contributions: 4
+        },
+        {
+            login: 'etherCorps',
+            avatar_url: 'https://avatars.githubusercontent.com/u/71087863',
+            contributions: 4
+        }
+    ];
+
+    let neighbors = $derived.by(() => getNavigationNeighbors(page.url.pathname));
 </script>
 
-<DocPage component={data.component} {...data.metadata} />
+<DocPage component={data.component} {...data.metadata} {neighbors} />
--- docs/src/routes/(docs)/docs/[...slug]/+page.svelte
+++ docs/src/routes/(docs)/docs/[...slug]/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import { DocPage } from "@svecodocs/kit";
+    import {getNavigationNeighbors} from "$lib/navigation";
+    import {page} from "$app/state";
 	let { data } = $props();
+    let neighbors = $derived.by(() => getNavigationNeighbors(page.url.pathname));
+
 </script>
 
-<DocPage component={data.component} {...data.metadata} />
+<DocPage component={data.component} {...data.metadata} {neighbors}/>
